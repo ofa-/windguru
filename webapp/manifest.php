c2f85b69028919db93b80410ba620fdc9cbf1725
ofa- <ofa@github>
2012-04-02 16:39:33 +0200

    add timestamp to manifest

--- webapp/manifest.php
+++ webapp/manifest.php
@@ -2,21 +2,26 @@
   header('Content-Type: text/cache-manifest');
   echo "CACHE MANIFEST\n";
 
-  $hashes = "";
-
+  $manifest = "./manifest.php";
+  $hashes   = "";
+  $mdate    = 0;
   $dir = new RecursiveDirectoryIterator(".");
   foreach(new RecursiveIteratorIterator($dir) as $file) {
-    if ($file->IsFile() &&
-        $file != "./manifest.php" &&
-        substr($file->getFilename(), 0, 1) != ".")
+    if (!$file->IsFile())
+	continue;
+    if (substr($file->getFilename(), 0, 1) == ".")
+	continue;
+    if ($file != $manifest)
     {
       echo substr($file, 2) . "\n";
       $hashes .= md5_file($file);
     }
+    $mdate = max($mdate, filemtime($file)); 
   }
   $extra = "manifest.extra";
   if (file_exists($extra))
 	  echo file_get_contents($extra) . "\n";
 
-  echo "# Hash: " . md5($hashes) . "\n";
+  $mdate =  date("Y-m-d H:i:s", filemtime($manifest));
+  echo "# Hash: " . md5($hashes) . " / $mdate\n";
 ?>
