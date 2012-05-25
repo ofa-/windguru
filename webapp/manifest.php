74c56843cdb6ec735e442c1a5f7be007ebc66dbc
ofa- <ofa@github>
2012-05-25 08:17:11 +0200

    reworked version info, dropped unused no_data.html

--- webapp/manifest.php
+++ webapp/manifest.php
@@ -2,8 +2,8 @@
   header('Content-Type: text/cache-manifest');
 
   $manifest = "./manifest.php";
-  $extra    = "manifest.extra";
-  $version  = "version.txt";
+  $extra    = "./manifest.extra";
+  $version  = "./version";
   $files    = "";
   $mdate    = 0;
   $dir = new RecursiveDirectoryIterator(".");
@@ -12,13 +12,14 @@
 	continue;
     if (substr($file->getFilename(), 0, 1) == ".")
 	continue;
-    if ($file != $manifest)
-      $files  .= substr($file, 2) . "\n";
+    if ($file == $manifest || $file == $version)
+	continue;
+    $files .= substr($file, 2) . "\n";
     $mdate = max($mdate, filemtime($file)); 
   }
   $date = date("Y-m-d H:i:s", $mdate);
   echo "CACHE MANIFEST\n";
-  echo "# Version: " . file_get_contents($version) . "\n";
+  echo "# Version: " . file_get_contents($version);
   echo "# Build  : $date\n";
   echo "$files\n";
   echo file_get_contents($extra) . "\n";
