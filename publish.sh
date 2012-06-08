15a14d998e2fdccf75d1ca9e6cd2ba3f91e1dc91
ofa- <ofa@github>
2012-06-08 18:40:52 +0200

    refactored

--- publish.sh
+++ publish.sh
@@ -1,25 +1,80 @@
-cd $(dirname $0)
-UPLOAD_SITE=".upload.site"
-if [ ! -f $UPLOAD_SITE ]; then
-	echo "URL='ftp://<user>:<pass>@<host>'" > $UPLOAD_SITE
-	echo "DIR=" >> $UPLOAD_SITE
-	echo "created sample upload site conf file: $UPLOAD_SITE"
-	echo "please edit and provide credentials and target dir."
-	exit 1
-fi
-. $UPLOAD_SITE
-[ -z "$URL" ] && { echo "URL not defined in $UPLOAD_SITE"; exit 1; }
-[ -z "$DIR" ] && { echo "DIR not defined in $UPLOAD_SITE"; exit 1; }
+#!/bin/bash
+CONFIG_FILE=".upload.site"
 
-LFTP_URL=$URL/$DIR
-if ! lftp $LFTP_URL -e "exit" 2> /dev/null ; then
-	lftp $URL -e "mkdir $DIR; exit" || exit 1
-fi
+function load_config() {
+	[ -f $CONFIG_FILE ]		|| { create_default_config; return 1; }
+	. $CONFIG_FILE			|| return 1
+	check_vars "URL DIR SRC"	|| return 1
+	[ -d "$SRC" ] 			|| error "$SRC: not a directory"
+}
 
-cd webapp
-version=$(cat version)
-major=${version%.*}
-minor=${version##*.}
-echo $major.$((minor+1)) > version
-lftp $LFTP_URL -e "mirror -X *.bak -X *.orig -Rn .; exit"
-echo "Published version: $(cat version)"
+function check_vars() {
+	for var in $@; do
+		[ -z "${!var}" ] && not_def="$not_def $var"
+	done
+	[ -z "$not_def" ] || error "no value defined for:$not_def"
+}
+
+function create_default_config() {
+	echo "URL='ftp://<user>:<pass>@<host>'" > $CONFIG_FILE
+	echo "DIR=		# target dir" >> $CONFIG_FILE
+	echo "SRC=webapp	# local  dir" >> $CONFIG_FILE
+	echo "created sample upload site conf file: $CONFIG_FILE"
+}
+
+function check_create_upload_dir() {
+	lftp $URL/$DIR -e "exit" 2> /dev/null	&& return 0
+	lftp $URL -e "mkdir $DIR; exit"		|| return 1
+}
+
+function abort() {
+	error $@ || exit 1
+}
+
+function error() {
+	echo $@ && return 1
+}
+
+function set_version() {
+	arg=$1
+
+	version=$(cat version)
+	major=${version%.*}
+	minor=${version##*.}
+	case $arg in
+		"")	return 0 ;;
+		-i)	version=$major.$((minor+1)) ;;
+		*)	version=$arg
+			parse_version || return 1 ;;
+	esac
+	echo $version > version
+}
+
+shopt -s extglob
+function parse_version() {
+	shopt -s extglob
+	case $version in
+		+([0-9]).+([0-9])) ;;
+		*) error "invalid version spec: $version" ;;
+	esac
+	ret=$?
+	shopt -u extglob
+	return $ret
+}
+shopt -u extglob
+
+function publish_local_directory() {
+	lftp $URL/$DIR -e "mirror -X *.bak -X *.orig -Rn .; exit" || return 1
+	echo "Published version: $(cat version)"
+}
+
+function main() {
+	cd $(dirname $0)
+	load_config		|| abort "please edit config file: $CONFIG_FILE"
+	check_create_upload_dir	|| abort "cannot create upload directory: $DIR"
+	cd $SRC
+	set_version $1		|| error "failed to set new version"
+	publish_local_directory || abort "failed to publish directory: $SRC"
+}
+
+main $1
